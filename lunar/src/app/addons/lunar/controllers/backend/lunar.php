f5358b4c225b5bb02ef8710544d04661aaeec276
lucionescu <lucian.ionesq@gmail.com>
2024-01-30 05:55:12 +0200

    Adjusted backend controller

--- lunar/src/app/addons/lunar/controllers/backend/lunar.php
+++ lunar/src/app/addons/lunar/controllers/backend/lunar.php
@@ -2,21 +2,26 @@
 
 if (!defined('BOOTSTRAP')) die('Access denied');
 
+if (!class_exists('\\Lunar\\Payment\\Transaction')) {
+    require_once(dirname(__DIR__, 2) . '/Transaction.php');
+}
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if ($mode == 'refund') {
+use Lunar\Payment\Transaction;
+
+if ($mode == 'refund') {
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $order_id = $_REQUEST['order_id'];
         $amount = $_REQUEST['amount'];
-
         $order_info = fn_get_order_info($order_id);
-        $txnId = !empty($order_info['payment_info']['transaction_id']) ? $order_info['payment_info']['transaction_id'] : '';
+        $transaction_id = !empty($order_info['payment_info']['transaction_id']) ? $order_info['payment_info']['transaction_id'] : '';
+
         if ($amount <= $order_info['total']) {
-            $cc = \lunar\Transaction::refund($order_info, $txnId, $amount);
+            Transaction::refund($order_info, $transaction_id, $amount);
         }
         return array(CONTROLLER_STATUS_OK, 'orders.details?order_id=' . $order_id);
-    }
-} else {
-    if ($mode == 'refund') {
+
+    } else {
         $order_id = $_REQUEST['order_id'];
         $order_info = fn_get_order_info($order_id);
         $captured_amount = !empty($order_info['payment_info']['captured_amount']) ? floatval($order_info['payment_info']['captured_amount']) : 0;
@@ -27,4 +32,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $view->assign('order_id', $order_id);
         $view->assign('amount', $captured_amount - $refunded_amount);
     }
+
 }
